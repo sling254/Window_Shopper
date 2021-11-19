31f1bd194babe6ebe3cf3127f8a0e22fec770e14
sling254 <mwanikidenis7@gmail.com>
2021-11-19 08:11:35 +0300

    change the database url in prod configurations

--- config.py
+++ config.py
@@ -29,7 +29,9 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 
 class DevConfig(Config):
